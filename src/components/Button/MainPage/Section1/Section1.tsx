880d7588f5512ff3d094987cea936addd7d4486e
Maksim <radchuk.maksime@gmail.com>
2024-10-26 14:16:43 +0300

    feature: добавил grid сетку на Section1 и Section2

--- src/components/Button/MainPage/Section1/Section1.tsx
+++ src/components/Button/MainPage/Section1/Section1.tsx
@@ -1,9 +1,26 @@
+import Button from "../../Button";
 import classSection1 from "./css/classSection1.module.css";
 
 const Section1 = () => {
   return (
     <section className={classSection1.section}>
-      <div className={classSection1.section_container}></div>
+      <div className={classSection1.section_container}>
+        <section className={classSection1.section1}></section>
+        <section className={classSection1.section2}></section>
+        <section className={classSection1.section3}></section>
+        <section className={classSection1.section4}>
+        <Button
+        type="button"
+        isIcon="calculate"
+        text="Рассчитать стоимость"
+        h={"63px"}
+        w={"343px"}
+        color={"btn1"}
+      />
+        </section>
+        <section className={classSection1.section5}></section>
+        <section className={classSection1.section6}></section>
+      </div>
     </section>
   );
 };
--- src/components/Button/MainPage/Section2/Section2.tsx
+++ src/components/Button/MainPage/Section2/Section2.tsx
@@ -1,9 +1,26 @@
+import Button from "../../Button";
 import classSection2 from "./css/classSection2.module.css";
 
 const Section2 = () => {
   return (
     <section className={classSection2.section}>
-      <div className={classSection2.section_container}></div>
+      <div className={classSection2.section_container}>
+        <section className={classSection2.section1}></section>
+        <section className={classSection2.section2}></section>
+        <section className={classSection2.section3}></section>
+        <section className={classSection2.section4}></section>
+        <section className={classSection2.section5}></section>
+        <section className={classSection2.section6}>
+        <Button
+        type="button"
+        text="Показать все"
+        h={"50px"}
+        w={"343px"}
+        color={"btn2"}
+      />
+        </section>
+
+      </div>
     </section>
   );
 };
