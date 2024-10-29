778312fa5ccb25078961256ceb66626707274033
Maksim <radchuk.maksime@gmail.com>
2024-10-29 17:51:48 +0300

    Нужно доработать ленивую загрузку и доконца разобраться со слайдерами

--- src/components/Button/MainPage/Section4/Components/Gallary.tsx
+++ src/components/Button/MainPage/Section4/Components/Gallary.tsx
@@ -1,77 +1,109 @@
-import classGallary from "./css/classGallary.module.css";
-// import img from "./../../../../../../public/galary/m1.jfif";
-import img1 from "./../../../../../../public/galary/m1.jfif";
-import img2 from "./../../../../../../public/galary/m2.jfif";
-import img3 from "./../../../../../../public/galary/m3.jfif";
-import img4 from "./../../../../../../public/galary/m4.jfif";
-import Button from "../../../Button";
-import { useRef, useState } from "react";
-// import IMGLoadingLazy from "../../../../IMG/IMGLazy";
-const arrayImg = [
-  { src: img1 },
-  { src: img2 },
-  { src: img3 },
-  { src: img4 },
-];
+// import classGallary from "./css/classGallary.module.css";
+// // import img from "./../../../../../../public/galary/m1.jfif";
+// import img1 from "./../../../../../../public/galary/m1.jfif";
+// import img2 from "./../../../../../../public/galary/m2.jfif";
+// import img3 from "./../../../../../../public/galary/m3.jfif";
+// import img4 from "./../../../../../../public/galary/m4.jfif";
+// import Button from "../../../Button";
+// import { useRef, useState } from "react";
+// import Carousel from "react-multi-carousel";
+// // import IMGLoadingLazy from "../../../../IMG/IMGLazy";
+// const arrayImg = [
+//   { src: img1 },
+//   { src: img2 },
+//   { src: img3 },
+//   { src: img4 },
+// ];
 
-const Gallary = () => {
-  const [numPicture, setNumPicture] = useState(0); 
-  const ref = useRef<HTMLDivElement>(null)
-  const scroll = ref.current?.scrollLeft;
-  // console.dir(ref.current?.scrollLeft);
-  const refChild = useRef<HTMLDivElement>(null)
-  // console.dir(refChild.current?.clientWidth);
-  const wElem =refChild.current?.clientWidth;
-  console.log(scroll,wElem);
-  // if (!scroll || !wElem) {
-  //   return
-  // }
-  // console.log(Math.round(scroll/wElem));
-  
-  
-  
-  // console.log(numPicture);
-  const changeNum =  (delta:number) =>{
-        setNumPicture(num=>num+delta)
-       
-        
-  }
-  return (<> <div className={classGallary.gallary}>
-    <div ref={ref} className={classGallary.gallary_container}>
+// const Gallary = () => {
+//   const [currentSlide, setCurrentSlide] = useState(0);
+//   const carouselRef = useRef<Carousel>(null);
+ 
+//   const responsive = {
+//     superLargeDesktop: {
+//       breakpoint: { max: 4000, min: 3000 },
+//       items: 5,
+//     },
+//     desktop: {
+//       breakpoint: { max: 3000, min: 1024 },
+//       items: 3,
+//     },
+//     tablet: {
+//       breakpoint: { max: 1024, min: 464 },
+//       items: 2,
+//     },
+//     mobile: {
+//       breakpoint: { max: 464, min: 0 },
+//       items: 1,
+//     },
+//   };
 
-{arrayImg.map((elem) => (
-    <div ref={refChild} className={classGallary.imgs} key={elem.src}>
-      {/* <IMGLoadingLazy> */}
-      <img  className={classGallary.img} src={elem.src} alt="" />
-      {/* </IMGLoadingLazy> */}
+//   const handleNext = () => {
+//     if (carouselRef.current) {
+//       carouselRef.current.next();
+//       console.log(carouselRef.current);
       
-    </div>
-  ))}
+//     }
+//   };
 
-    </div>
-    <Button
-        method={()=>changeNum(1)}
-        numPicture={numPicture==arrayImg.length-1}
-        type="button"
-        isIcon="pagRLight"
-        h={"44px"}
-        w={"44px"}
-        color={"btn5"}
-      />
-<Button
-method={()=>changeNum(-1)}
-      numPicture = {numPicture ==0 }
-        type="button"
-        isIcon="pagLLight"
-        h={"44px"}
-        w={"44px"}
-        color={"btn5"}
-      />
-</div>
+//   const handlePrev = () => {
+//     if (carouselRef.current) {
+//       carouselRef.current.previous();
+//       console.log("prev");
+      
+//     }
+//   };
 
- </>
+//   const ref = useRef<HTMLDivElement>(null)
+//   const scroll = ref.current?.scrollLeft;
+//   // console.dir(ref.current?.scrollLeft);
+//   const refChild = useRef<HTMLDivElement>(null)
+//   // console.dir(refChild.current?.clientWidth);
+//   const wElem =refChild.current?.clientWidth;
+//   console.log(scroll,wElem);
+  
+  
+  
+  
+
+//   return (<> <div className={classGallary.gallary}>
+//     <div ref={ref} className={classGallary.gallary_container}>
+//     <Carousel ref={carouselRef} arrows={false} responsive={responsive}>
+//     {arrayImg.map((elem) => (
+//     <div ref={refChild} className={classGallary.imgs} key={elem.src}>
+//       {/* <IMGLoadingLazy> */}
+//       <img  className={classGallary.img} src={elem.src} alt="" />
+//       {/* </IMGLoadingLazy> */}
+      
+//     </div>
+//   ))}
+//           </Carousel>
+
+
+//     </div>
+//     <Button
+//             method={handleNext}
+//             numPicture={currentSlide === arrayImg.length - 1}
+//             type="button"
+//             isIcon="pagRLight"
+//             h={"44px"}
+//             w={"44px"}
+//             color={"btn5"}
+//           />
+//           <Button
+//             method={handlePrev}
+//             // numPicture={currentSlide === 0}
+//             type="button"
+//             isIcon="pagLLight"
+//             h={"44px"}
+//             w={"44px"}
+//             color={"btn5"}
+//           />
+// </div>
+
+//  </>
       
    
-  );
-};
-export default Gallary;
+//   );
+// };
+// export default Gallary;
--- src/components/Button/MainPage/Section4/Section4.tsx
+++ src/components/Button/MainPage/Section4/Section4.tsx
@@ -1,17 +1,104 @@
 
-import Gallary from "./Components/Gallary";
 import classSection4 from "./css/classSection4.module.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import img1 from "./../../../../../public/galary/m1.jfif";
+import img2 from "./../../../../../public/galary/m2.jfif";
+import img3 from "./../../../../../public/galary/m3.jfif";
+import img4 from "./../../../../../public/galary/m4.jfif";
+import {  useRef, useState} from "react";
+import Button from "../../Button";
+
+const arrayImg = [
+  { src: img1 },
+  { src: img2 },
+  { src: img3 },
+  { src: img4 },
+];
 
 const Section4 = () => {
+  const carouselRef = useRef<Carousel>(null);
+  const [state, setState] = useState<number>(0)
+  console.log(state);
+  
+ 
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  const handleNext = () => {
+    if (carouselRef.current) {
+      carouselRef.current.next(0);
+      if (state+1==arrayImg.length) {
+        return state
+      }
+      setState(s=>s+1)
+      
+     
+      
+    }
+  };
+
+  const handlePrev = () => {
+    if (carouselRef.current) {
+      carouselRef.current.previous(0);
+      if (state-1==-1) {
+        return state
+      }
+      setState(s=>s-1)
+    }
+  };
+
+
   return (
     <section className={classSection4.section}>
       <div className={classSection4.section_container}>
         <section className={classSection4.section1}></section>
         <section className={classSection4.section2}>
-          <Gallary />
+          <Carousel  ref={carouselRef} arrows={false} responsive={responsive}>
+            {arrayImg.map((elem) => (
+              <div className={classSection4.imgs} key={elem.src}>
+                <img className={classSection4.img} src={elem.src} alt="" />
+              </div>
+            ))}
+          </Carousel>
         </section>
         <section className={classSection4.section3}>
-          
+        
+          <Button
+          numPicture={state===0}
+            method={()=>handlePrev()}
+            type="button"
+            isIcon="pagLLight"
+            h={"44px"}
+            w={"44px"}
+            color={"btn5"}
+          />
+            <Button
+            numPicture={state===arrayImg.length-1}
+
+            method={()=>handleNext()}
+            type="button"
+            isIcon="pagRLight"
+            h={"44px"}
+            w={"44px"}
+            color={"btn5"}
+          />
         </section>
       </div>
     </section>
